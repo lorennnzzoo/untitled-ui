117377a60f44e10f9cf597a00c4c8fcf8a7814e6
loren <lorenbhanu@gmail.com>
2025-09-15 00:20:47 +0530

    implement sidebar toggle functionality and update layout structure

--- src/app/layouts/main/main.ts
+++ src/app/layouts/main/main.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { Sidebar } from "../../sidebar/sidebar";
 import { Footer } from "../../footer/footer";
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [Sidebar, RouterOutlet],
+  imports: [Sidebar, RouterOutlet,CommonModule],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
 export class Main {
+  isSidebarOpen:boolean = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
